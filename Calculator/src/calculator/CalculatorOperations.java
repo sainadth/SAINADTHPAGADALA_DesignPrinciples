842fd97f057c9b2c2a8dc78256cf7fb1230f7851
sainadth <40690148+sainadth@users.noreply.github.com>
2020-02-10 23:08:08 +0530

    Update CalculatorOperations.java

--- Calculator/src/calculator/CalculatorOperations.java
+++ Calculator/src/calculator/CalculatorOperations.java
@@ -2,7 +2,7 @@ package calculator;
 
 
 public class CalculatorOperations {
-    public static double evaluateExpression(int number1, int number2, char operator){
+    public static double evaluateExpression(double number1, double number2, char operator){
         switch(operator){
             case '+': return number1+number2;
             case '-': return number1-number2;
